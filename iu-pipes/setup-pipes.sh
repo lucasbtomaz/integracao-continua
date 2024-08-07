39eff0971e6a6e295736ebdec6230a44714f4ad5
Lucas Bento Tomaz <olucastomaz@hotmail.com>
2024-08-06 21:31:16 -0300

    Initial setup with CI, Lambda, RDS simulation, Prometheus & Grafana, IU Pipes 2.0, and Terraform configurations

---
+++ iu-pipes/setup-pipes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Script de configuração do IU Pipes 2.0
+# Adicione aqui os comandos necessários para configurar o IU Pipes 2.0
+echo "Configuração do IU Pipes 2.0 iniciada"
+# Exemplo de comando de configuração:
+# ./iu-pipes-cli setup --config config.yaml
+echo "Configuração do IU Pipes 2.0 concluída"
